d9ba0e730a7738f35c05ce9d0408bd64c8334d7c
unknown <ajaymore0634@gmail.com>
2023-05-05 12:32:10 +0530

    index page done

--- CarPoint/urls.py
+++ CarPoint/urls.py
@@ -5,5 +5,17 @@ from CarPoint import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.index,name='index')
+    path("",views.index,name='index'),
+    path('buyingPrice/',views.buyingPrice,name='buy price'),
+    path('sellingPrice/',views.sellingPrice,name='sell price'),
+    path('model1/',views.firstModel,name='model 1'),
+    path('model2/',views.secondModel,name='model 2'),
+    path('model3/',views.thiredModel,name='model 3'),
+    path('dataset1/',views.dataset1,name='dataset 1'),
+    path('dataset2/',views.dataset2,name='dataset 2'),
+    path('dataset3/',views.dataset3,name='dataset 3'),
+    path('dataset4/',views.dataset4,name='dataset 4'),
+    path('dataset5/',views.dataset5,name='dataset 5'),
+    path('aboutUs/',views.aboutUs,name='about us'),
+    path('discover/',views.discover,name='discover'),
 ]
--- CarPoint/views.py
+++ CarPoint/views.py
@@ -3,3 +3,42 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def buyingPrice(request):
+    return HttpResponse("<a href='/aboutUs'>click Me</a>")
+
+def sellingPrice(request):
+    return HttpResponse("It Is selling Price")
+
+def firstModel(request):
+    return HttpResponse("It Is Model 1")
+
+def secondModel(request):
+    return HttpResponse("It Is Model 2")
+
+def thiredModel(request):
+    return HttpResponse("It Is Model 3")
+
+def dataset1(request):
+    return HttpResponse("It Is dataset 1")
+
+def dataset2(request):
+    return HttpResponse("It Is dataset 2")
+
+def dataset3(request):
+    return HttpResponse("It Is dataset 3")
+
+def dataset4(request):
+    return HttpResponse("It Is dataset 4")
+
+def dataset5(request):
+    return HttpResponse("It Is dataset 5")
+
+def discover(requet):
+    return HttpResponse("Discover")
+
+def aboutUs(request):
+    return HttpResponse("about us")
+
+
+
